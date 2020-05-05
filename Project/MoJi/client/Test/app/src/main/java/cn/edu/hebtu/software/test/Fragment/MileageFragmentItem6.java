aae2507fa1238f13d5a69bec77f2646e54294a88
Levi9420 <37906030+Levi9420@users.noreply.github.com>
2020-05-05 20:13:57 +0800

    Add details of the MileageFragment1 by Zhence Di

---
+++ Project/MoJi/client/Test/app/src/main/java/cn/edu/hebtu/software/test/Fragment/MileageFragmentItem6.java
@@ -0,0 +1,25 @@
+package cn.edu.hebtu.software.test.Fragment;
+
+import cn.edu.hebtu.software.test.R;
+
+/**
+ * @Author: 邸祯策
+ * @Date: 2020/05/05
+ * @Describe: 视频推荐
+ */
+public class MileageFragmentItem6 extends MyBaseFragment {
+    @Override
+    protected void lazyLoad() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected int getContentViewId() {
+        return  R.layout.mileage_layout_item6;
+    }
+}
